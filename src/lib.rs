15449e8dde15fa451ff61051e9b7d7456e446c8f
Ben Simms <ben@bensimms.moe>
2020-07-09 13:25:59 +0100

    Get the other side working

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,41 @@ fn rust_side_worker() {
     }
 }
 
+#[pyclass]
+struct EventCompleter {
+    s: async_channel::Sender<PyObject>,
+}
+
+#[pymethods]
+impl EventCompleter {
+    #[call]
+    #[args(val)]
+    fn __call__(&self, val: PyObject) -> PyResult<()> {
+        let _ = self.s.try_send(val);
+
+        Ok(())
+    }
+}
+
+async fn wait_for_py_coro_impl(loop_: PyObject, coro: PyObject) -> PyResult<PyObject> {
+    let r = {
+        let gil = Python::acquire_gil();
+        let py = gil.python();
+
+        let (s, r) = async_channel::bounded(1);
+
+        let fut = loop_.call_method1(py, "create_task", (coro,))?;
+
+        let callback = PyCell::new(py, EventCompleter { s })?;
+
+        fut.call_method1(py, "add_done_callback", (callback,))?;
+
+        r
+    };
+
+    r.recv().await.map_err(|_| pyo3::exceptions::RuntimeError::py_err("future dropped"))
+}
+
 fn set_fut_result(loop_: PyObject, fut: PyObject, res: PyObject) -> PyResult<()> {
     let gil = Python::acquire_gil();
     let py = gil.python();
@@ -29,14 +64,46 @@ fn set_fut_result(loop_: PyObject, fut: PyObject, res: PyObject) -> PyResult<()>
     Ok(())
 }
 
+fn get_loop(py: Python) -> PyResult<PyObject> {
+    let asyncio = PyModule::import(py, "asyncio")?;
+    let loop_ = asyncio.call0("get_running_loop")?;
+
+    Ok(loop_.into())
+}
+
+#[pyfunction]
+fn wait_for_py_coro(fut: PyObject) -> PyResult<()> {
+    let loop_ = {
+        let gil = Python::acquire_gil();
+        let py = gil.python();
+        get_loop(py)?
+    };
+
+    smol::Task::spawn(async move {
+        let r = wait_for_py_coro_impl(loop_, fut).await;
+        match r {
+            Ok(v) => {
+                println!("wfpc got Ok: {:?}", v);
+            }
+            Err(e) => {
+                eprintln!("waiting on python coro failed");
+                let gil = Python::acquire_gil();
+                let py = gil.python();
+                e.print(py);
+            }
+        }
+    }).detach();
+
+    Ok(())
+}
+
 #[pyfunction]
 fn delay_test(delay: u64, result: PyObject) -> PyResult<PyObject> {
     let (fut, res_fut, loop_): (PyObject, PyObject, PyObject) = {
         let gil = Python::acquire_gil();
         let py = gil.python();
-        let asyncio = PyModule::import(py, "asyncio")?;
-        let loop_ = asyncio.call0("get_running_loop")?;
-        let fut: PyObject = loop_.call_method0("create_future")?.into();
+        let loop_ = get_loop(py)?;
+        let fut: PyObject = loop_.call_method0(py, "create_future")?.into();
         (fut.clone_ref(py), fut, loop_.into())
     };
 
@@ -60,6 +127,8 @@ fn delay_test(delay: u64, result: PyObject) -> PyResult<PyObject> {
 fn async_py_rust(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(rust_side_worker))?;
     m.add_wrapped(wrap_pyfunction!(delay_test))?;
+    m.add_wrapped(wrap_pyfunction!(wait_for_py_coro))?;
+    m.add_class::<EventCompleter>()?;
 
     Ok(())
 }
--- test.py
+++ test.py
@@ -3,11 +3,19 @@ import asyncio
 
 async_py_rust.rust_side_worker()
 
+async def rust_side_test():
+    print("rst before")
+    await asyncio.sleep(3)
+    print("rst after")
+    return "hi from py"
+
 async def t():
     print("hi")
-    fut = async_py_rust.delay_test(3, "lol")
-    print(fut)
-    r = await fut
-    print(r)
+    r = await async_py_rust.delay_test(3, "lol")
+    print(f"rust coro completed returning")
+    print("waiting on python coro from rust")
+    async_py_rust.wait_for_py_coro(rust_side_test())
+    print("done, waiting to keep the loop alive")
+    await asyncio.sleep(7)
 
 asyncio.run(t())
